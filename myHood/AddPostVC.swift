36542e496512c636d148fa1397997999e74242fa
Alex Lombry <alex.lombry@gmail.com>
2015-10-11 01:24:19 +0200

    add camera image and change constraint on it

--- myHood/AddPostVC.swift
+++ myHood/AddPostVC.swift
@@ -8,32 +8,54 @@
 
 import UIKit
 
-class AddPostVC: UIViewController {
+class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var descriptionField: UITextField!
     
     @IBOutlet weak var postImage: UIImageView!
     
+    var imgPicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         
         // Corner radius for image and delete everything above
-         postImage.layer.cornerRadius = postImage.frame.size.width / 2
-         postImage.clipsToBounds = true
+        postImage.layer.cornerRadius = postImage.frame.size.width / 2
+        postImage.clipsToBounds = true
         
-        // Do any additional setup after loading the view.
+        // Load picker
+        imgPicker = UIImagePickerController()
+        imgPicker.delegate = self
+        
+        
     }
     
     @IBAction func addPicBtnPressed(sender: UIButton!) {
         sender.setTitle("", forState: .Normal)
+        presentViewController(imgPicker, animated: true, completion: nil)
     }
     
     @IBAction func makePostBtnPressed(sender: AnyObject) {
-    
+        
+        // Let's make sure we got all data
+        if let title = titleField.text, let desc = descriptionField.text, let img = postImage.image {
+            
+        } else {
+            
+        }
+        
     }
 
     @IBAction func cancelBtnPressed(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    // Automaticly call by delegate, set the image into postImage
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imgPicker.dismissViewControllerAnimated(true, completion: nil)
+        postImage.image = image
+        
     }
 }
