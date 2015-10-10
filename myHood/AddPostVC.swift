cac803108336efcc6f3792bb47c4cc62b5d14eda
Alex Lombry <alex.lombry@gmail.com>
2015-10-11 00:54:48 +0200

    fix round post Image

--- myHood/AddPostVC.swift
+++ myHood/AddPostVC.swift
@@ -10,10 +10,30 @@ import UIKit
 
 class AddPostVC: UIViewController {
 
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
+    
+    @IBOutlet weak var postImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Corner radius for image and delete everything above
+         postImage.layer.cornerRadius = postImage.frame.size.width / 2
+         postImage.clipsToBounds = true
+        
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func addPicBtnPressed(sender: UIButton!) {
+        sender.setTitle("", forState: .Normal)
+    }
+    
+    @IBAction func makePostBtnPressed(sender: AnyObject) {
+    
+    }
 
+    @IBAction func cancelBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
